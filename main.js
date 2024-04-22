0a6bd1c4eee84676d218ee26c11554c81b6d8762
Salma Ali <160733107+salma6020@users.noreply.github.com>
2024-04-22 15:51:17 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let todoquestions = await inquirer.prompt([
+        {
+            name: "firstQuestion",
+            type: "input",
+            message: "what would you like to add in you todos?"
+        },
+        {
+            name: "secondQuestion",
+            type: "confirm",
+            message: " would you like to add in you todos?",
+            default: "true"
+        }
+    ]);
+    todos.push(todoquestions.firstQuestion);
+    console.log(todos);
+    //the loop is running on the based of this variable condition
+    condition = todoquestions.secondQuestion;
+}
+//read,update,delete, add
+const fruits = ['apple', 'orange', 'banana', 'kiwi'];
+fruits.forEach(fruit => {
+    console.log(fruits);
+});
